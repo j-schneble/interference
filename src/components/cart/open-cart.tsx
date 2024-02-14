81735e5fcf9d9b8ee5745e25a349ccef275c9f5b
Jack Schneble <schneble.jack@gmail.com>
2024-02-14 18:53:44 -0500

    Correction for deployment

--- src/components/cart/open-cart.tsx
+++ src/components/cart/open-cart.tsx
@@ -1,5 +1,5 @@
 import { IoBagOutline } from "react-icons/io5";
-import clsx from 'clsx';
+// import clsx from 'clsx';
 
 export default function OpenCart({
   className,
@@ -10,9 +10,9 @@ export default function OpenCart({
 }) {
   return (
     <div className="relative flex items-center justify-center p-2 text-white transition-colors border rounded-md border-neutral-200 ">
-   <IoBagOutline
+   {/* <IoBagOutline
         className={clsx('h-5 w-5 transition-all ease-in-out hover:scale-110 ', className)}
-      />
+      /> */}
 
       {quantity ? (
         <div className="absolute right-0 top-0 -mr-2 -mt-2 h-8 w-8 rounded bg-black text-[11px] font-medium text-white">
